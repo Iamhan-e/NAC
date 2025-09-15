19d9eda7dd8e37cc1996ecd34756b042f00a7250
Iamhan-e <h2anaschalew@gmail.com>
2025-09-15 21:25:17 +0300

    Refactor server routes to use '/api' prefix and remove body-parser dependency

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,14 @@
 const express= require('express');
-const bodyParser= require('body-parser');
 const authRoutes= require('./auth/authRoutes')
 
 const app= express()
 const PORT= 3000;
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 // routes
 
-app.use('/auth', authRoutes);
+app.use('/api', authRoutes);
 
 app.listen(PORT, ()=>{
     console.log(`Server running on http://localhost:${PORT}`)
